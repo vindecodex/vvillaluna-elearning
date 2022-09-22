2cb8885413e6176d0501f4caf15b8cf8cc4342ca
Vincent Villaluna <villalunavincent@gmail.com>
2022-09-22 13:49:24 +0800

    Service: Implement Get Users and by ID of user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+  async findAll() {
+    const users = await this.userRepo.find();
+    return {
+      data: users,
+      totalCount: 1,
+      page: 1,
+      limit: 5,
+    };
+  }
+
+  async findOne(id: string) {
+    try {
+      const user = await this.userRepo.findOne({ where: { id } });
+      return { ...user };
+    } catch (e) {
+      console.log(e);
+      if (e.code === '22P02') {
+        throw new BadRequestException('Invalid ID format provided');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
