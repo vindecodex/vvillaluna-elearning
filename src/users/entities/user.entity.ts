eba621e3fdf1f7d2426116b8666dc805585ba0a3
Vincent Villaluna <villalunavincent@gmail.com>
2022-09-22 12:29:38 +0800

    Module: Initial user setup

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,42 @@
+import { IsEmail } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  salt: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  role: string;
+
+  @Column()
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
