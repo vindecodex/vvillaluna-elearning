cf067c31857afba7adfe6e84742c8c16f0bd54a4
Vincent Villaluna <villalunavincent@gmail.com>
2022-09-22 13:48:00 +0800

    Controller: Handle Get Users and By User ID endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+}
