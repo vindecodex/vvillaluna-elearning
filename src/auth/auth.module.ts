15af3effe5e73a862319530855880ac50e6807ad
Vince Villaluna <villalunavincent@gmail.com>
2022-09-23 19:34:59 +0800

    Module: Import JWT Strategy and setup config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MailModule } from 'src/providers/mail/mail.module';
 import { User } from 'src/users/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './providers/jwt/jwt.strategy';
+import { MailModule } from './providers/mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), MailModule],
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: 3600 * 24 },
+      }),
+    }),
+    TypeOrmModule.forFeature([User]),
+    MailModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
