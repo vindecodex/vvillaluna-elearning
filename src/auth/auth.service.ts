36b8b6d8586b6bcc2037f022d8843c07032b5800
Vince Villaluna <villalunavincent@gmail.com>
2022-09-23 19:38:20 +0800

    Auth: Sign token to be sent on email confirmation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,13 +8,16 @@ import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { UserCredentialsDto } from './dto/user-credentials.dto';
 import * as bcrypt from 'bcrypt';
-import { MailService } from 'src/providers/mail/mail.service';
+import { JwtService } from '@nestjs/jwt';
+import { MailService } from './providers/mail/mail.service';
+import { JwtPayload } from './providers/jwt/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private userRepo: Repository<User>,
     private mailService: MailService,
+    private jwtService: JwtService,
   ) {}
 
   async create(userCredentialsDto: UserCredentialsDto) {
@@ -33,8 +36,18 @@ export class AuthService {
         isActive: false,
       });
 
+      const jwtPayload: JwtPayload = {
+        id: user.id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        role: user.role,
+      };
+
+      const verificationToken = this.jwtService.sign(jwtPayload);
+
       await this.userRepo.save(user);
-      await this.mailService.sendVerification(user, '$token_sample$');
+      await this.mailService.sendVerification(user, verificationToken);
 
       return user;
     } catch (e) {
