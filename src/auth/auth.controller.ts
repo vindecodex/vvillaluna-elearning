1bb6bd3886b4d16852af6fff779a72086bf8527a
Vince Villaluna <villalunavincent@gmail.com>
2022-09-23 21:01:54 +0800

    Controller: Handle resend verification

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,6 +11,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/decorators/get-user.decorators';
 import { User } from 'src/users/entities/user.entity';
 import { AuthService } from './auth.service';
+import { ResendVerificationDto } from './dto/resend-verification.dto';
 import { UserCredentialsDto } from './dto/user-credentials.dto';
 
 @Controller()
@@ -27,5 +28,10 @@ export class AuthController {
   @UseGuards(AuthGuard('jwt'))
   verifyAccount(@GetUser() user: User) {
     return this.authService.verifyUser(user);
+  }
+
+  @Post('signup/verification')
+  resendVerification(@Body() resendVerificationDto: ResendVerificationDto) {
+    return this.authService.resendVerificationEmail(resendVerificationDto);
   }
 }
