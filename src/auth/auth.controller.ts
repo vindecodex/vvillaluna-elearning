1f03e8ed935daffc0ba8a37e489890866fda165e
Vince Villaluna <villalunavincent@gmail.com>
2022-09-23 20:01:31 +0800

    Controller: Handle account verification

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,9 +2,14 @@ import {
   Body,
   ClassSerializerInterceptor,
   Controller,
+  Get,
   Post,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/decorators/get-user.decorators';
+import { User } from 'src/users/entities/user.entity';
 import { AuthService } from './auth.service';
 import { UserCredentialsDto } from './dto/user-credentials.dto';
 
@@ -14,7 +19,13 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() userCredentialsDto: UserCredentialsDto) {
-    return this.authService.create(userCredentialsDto);
+  createUser(@Body() userCredentialsDto: UserCredentialsDto) {
+    return this.authService.createUser(userCredentialsDto);
+  }
+
+  @Get('signup/verification')
+  @UseGuards(AuthGuard('jwt'))
+  verifyAccount(@GetUser() user: User) {
+    return this.authService.verifyUser(user);
   }
 }
