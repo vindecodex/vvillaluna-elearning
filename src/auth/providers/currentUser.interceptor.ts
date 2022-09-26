f74edf7d068c3aaf2402d5474d815e253235695d
Vince Villaluna <villalunavincent@gmail.com>
2022-09-26 18:57:22 +0800

    Provider: Added interceptor to parse jwt from cookie

---
+++ src/auth/providers/currentUser.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private jwtService: JwtService) {}
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest();
+    if (request.cookies) {
+      request.user = this.jwtService.decode(request.cookies['token']);
+    }
+    return next.handle();
+  }
+}
