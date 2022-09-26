0b36c3aaf80183a58b9e543c26e4858783151dab
Vince Villaluna <villalunavincent@gmail.com>
2022-09-26 14:15:43 +0800

    Provider: Add mail service for account not found (Also fix naming for mailerServer to mailerService)

--- src/auth/providers/mail/mail.service.ts
+++ src/auth/providers/mail/mail.service.ts
@@ -4,14 +4,23 @@ import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class MailService {
-  constructor(private mailerServer: MailerService) {}
+  constructor(private mailerService: MailerService) {}
 
   async sendVerification(user: User, token: string) {
     const activitionLink = `http://localhost:3000/signup/verification?token=${token}`;
-    const res = await this.mailerServer.sendMail({
+    const res = await this.mailerService.sendMail({
       to: user.email,
       subject: 'Account Verification',
       html: `Click this link to <a href="${activitionLink}">verify account</a>`,
+    });
+    return res;
+  }
+
+  async sendAccountNotFound(email: string) {
+    const res = await this.mailerService.sendMail({
+      to: email,
+      subject: 'Account Not Found',
+      html: `Sorry your email: ${email} is not in our records`,
     });
     return res;
   }
