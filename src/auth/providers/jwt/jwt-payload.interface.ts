1aadb74851c00364f741b874d8c3d73b9178bb37
Vince Villaluna <villalunavincent@gmail.com>
2022-09-26 20:36:56 +0800

    Enhancement: Set id as required field for jwt-payload

--- src/auth/providers/jwt/jwt-payload.interface.ts
+++ src/auth/providers/jwt/jwt-payload.interface.ts
@@ -1,7 +1,7 @@
 export interface JwtPayload {
   id: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  role: string;
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  role?: string;
 }
