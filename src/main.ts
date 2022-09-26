8c2766068b444d8ecb9bc9ad59c6c04a767bf775
Vince Villaluna <villalunavincent@gmail.com>
2022-09-26 18:56:53 +0800

    APP: use cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
