9eba0b4c4a2d9a48e84a6ea5c39918ef50a427b6
Vince Villaluna <villalunavincent@gmail.com>
2022-10-27 20:58:39 +0800

    Test: enrollment service

--- src/enrollment/service/enrollment.service.spec.ts
+++ src/enrollment/service/enrollment.service.spec.ts
@@ -5,6 +5,9 @@ import { Module } from '../../module/entities/module.entity';
 import { Repository } from 'typeorm';
 import { Enrollment } from '../entities/enrollment.entity';
 import { EnrollmentService } from './enrollment.service';
+import { User } from '../../user/entities/user.entity';
+import { PostgresErrorCode } from '../../shared/enums/error-code/postgres.enum';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 type MockType<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 type MockEnrollmentRepo = MockType;
@@ -13,22 +16,19 @@ const mockEnrollmentRepo = (): MockEnrollmentRepo => ({
   create: jest.fn(),
   update: jest.fn(),
   delete: jest.fn(),
+  save: jest.fn(),
 });
 
 type MockEnrollmentModuleRepo = MockType;
 const mockEnrollmentModuleRepo = (): MockEnrollmentModuleRepo => ({
-  findOne: jest.fn(),
-  create: jest.fn(),
-  update: jest.fn(),
-  delete: jest.fn(),
+  findOneBy: jest.fn(),
+  save: jest.fn(),
+  preload: jest.fn(),
 });
 
 type MockModuleRepo = MockType;
 const mockModuleRepo = (): MockModuleRepo => ({
-  findOne: jest.fn(),
-  create: jest.fn(),
-  update: jest.fn(),
-  delete: jest.fn(),
+  findBy: jest.fn(),
 });
 
 describe('EnrollmentService', () => {
@@ -66,7 +66,95 @@ describe('EnrollmentService', () => {
     moduleRepo = module.get<MockModuleRepo>(getRepositoryToken(Module));
   });
 
+  const enrollment = new Enrollment();
+  const enrollmentModule = new EnrollmentModule();
+  const module = new Module();
+  const user = new User();
+
   it('should be defined', () => {
     expect(enrollmentService).toBeDefined();
+  });
+
+  describe('enrollmentService.create', () => {
+    it('should return created enrollment', async () => {
+      enrollmentRepo.create.mockResolvedValue(enrollment);
+      moduleRepo.findBy.mockResolvedValue([module]);
+      enrollmentModuleRepo.save.mockResolvedValue(true);
+      enrollmentRepo.save.mockResolvedValue(true);
+      const result = await enrollmentService.create({ courseId: 1 }, user);
+      expect(result).toEqual(enrollment);
+    });
+
+    it('should throw BadRequestException if already enrolled', async () => {
+      enrollmentRepo.create.mockResolvedValue(enrollment);
+      moduleRepo.findBy.mockResolvedValue([module]);
+      enrollmentModuleRepo.save.mockResolvedValue(true);
+      enrollmentRepo.save.mockRejectedValue({
+        code: PostgresErrorCode.DUPLICATE,
+      });
+      await expect(
+        enrollmentService.create({ courseId: 1 }, user),
+      ).rejects.toThrow(BadRequestException);
+    });
+
+    it('should throw NotFoundException if course is not found', async () => {
+      enrollmentRepo.create.mockResolvedValue(enrollment);
+      moduleRepo.findBy.mockResolvedValue([module]);
+      enrollmentModuleRepo.save.mockResolvedValue(true);
+      enrollmentRepo.save.mockRejectedValue({
+        code: PostgresErrorCode.INVALID_RELATION_KEY,
+      });
+      await expect(
+        enrollmentService.create({ courseId: 1 }, user),
+      ).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('enrollmentService.findOne', () => {
+    it('should return enrollment', async () => {
+      enrollmentRepo.findOne.mockResolvedValue(enrollment);
+      const result = await enrollmentService.findOne(1);
+      expect(result).toEqual(enrollment);
+    });
+
+    it('should throw NotFoundException if enrollment is not found', async () => {
+      enrollmentRepo.findOne.mockResolvedValue(false);
+      await expect(enrollmentService.findOne(1)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('enrollmentService.update', () => {
+    it('should return the updated enrollment', async () => {
+      const updatedEnrollment = enrollmentModule;
+      enrollmentModuleRepo.findOneBy.mockResolvedValue(true);
+      enrollmentModuleRepo.preload.mockResolvedValue(updatedEnrollment);
+      enrollmentModuleRepo.save.mockResolvedValue(updatedEnrollment);
+      const result = await enrollmentService.update(1, {
+        isCompleted: true,
+        moduleId: 1,
+      });
+      expect(result).toEqual(updatedEnrollment);
+    });
+    it('should throw NotFoundException if enrollment module not found', async () => {
+      enrollmentModuleRepo.findOneBy.mockResolvedValue(true);
+      enrollmentModuleRepo.preload.mockResolvedValue(true);
+      enrollmentModuleRepo.save.mockRejectedValue({
+        code: PostgresErrorCode.INVALID_RELATION_KEY,
+      });
+      await expect(
+        enrollmentService.update(1, { isCompleted: true, moduleId: 1 }),
+      ).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('enrollmentService.delete', () => {
+    it('should throw NotFoundException if no affected rows on deletion', async () => {
+      enrollmentRepo.delete.mockResolvedValue({ affected: 0 });
+      await expect(enrollmentService.delete(1)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
   });
 });
