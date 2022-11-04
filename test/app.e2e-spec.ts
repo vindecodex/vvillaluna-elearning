66b86cdc8aaab7f0cf1d9691749dc54e61d0accc
Vince Villaluna <villalunavincent@gmail.com>
2022-11-04 11:56:08 +0800

    E2E: App service

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  HttpStatus,
+  INestApplication,
+} from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { Reflector } from '@nestjs/core';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -12,13 +17,21 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalInterceptors(
+      new ClassSerializerInterceptor(app.get(Reflector)),
+    );
+
     await app.init();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(HttpStatus.OK)
+      .expect('Welcome to E-Learning API');
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 });
